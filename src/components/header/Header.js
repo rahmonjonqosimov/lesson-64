06d6ca7524cf2e8b57725d51e1b2268a801bf66a
QosimovRahmonjonn <qosimovrahmonjon2023@gmail.com>
2024-04-09 16:21:59 +0500

    navbar toggle

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,6 +4,7 @@ import bxX from "../../assets/images/bx-x.svg";
 
 const Header = () => {
   let linksData = ["Home", "About", "Blog", "Contact", "Help"];
+  const [menuOpen, setMenuopen] = useState(false);
   let links = linksData.map((el, i) => (
     <li key={i}>
       <a href="#" className="nav__links">
@@ -11,21 +12,6 @@ const Header = () => {
       </a>
     </li>
   ));
-  const toogle = (e) => {
-    if (
-      e.target.className === "menu" ||
-      e.target.className === "menu btn-menu"
-    ) {
-      let el = e.target.previousElementSibling;
-      if (el.className === "links") {
-        el.classList.add("show");
-        e.target.classList.add("btn-menu");
-      } else {
-        el.classList.remove("show");
-        e.target.classList.remove("btn-menu");
-      }
-    }
-  };
   return (
     <section id="header">
       <div className="container">
@@ -33,10 +19,19 @@ const Header = () => {
           <a href="#" className="logo">
             грузовик
           </a>
-          <ul className="links">{links}</ul>
-          <div onClick={toogle} className="menu">
-            <img src={btnMenu} alt="menu" className="btn-1" />
-            <img src={bxX} alt="X" className="btn-2" />
+          <ul className={menuOpen ? "links show" : "links"}>{links}</ul>
+          <div
+            onClick={() => {
+              setMenuopen(!menuOpen);
+            }}
+            className="menu"
+          >
+            <img
+              src={btnMenu}
+              alt="menu"
+              className={menuOpen ? "btn-1" : "btn"}
+            />
+            <img src={bxX} alt="X" className={!menuOpen ? "btn-1" : "btn"} />
           </div>
         </div>
       </div>
